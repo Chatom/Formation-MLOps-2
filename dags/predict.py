37ec127675cf45cca6b42e457e2847a7f1318db8
chatom <tom.chassagne@gmail.com>
2026-05-05 09:40:51 +0000

    Update

--- dags/predict.py
+++ dags/predict.py
@@ -7,7 +7,7 @@ from airflow.decorators import dag, task
 
 sys.path.insert(0, os.path.abspath(os.path.dirname(os.path.dirname(__file__))))  # So that airflow can find config files
 
-from dags.config import GENERATED_DATA_PATH, DATA_FOLDER
+from dags.config import GENERATED_DATA_PATH, MODEL_PATH, DATA_FOLDER, PREDICTIONS_FOLDER
 from formation_indus_ds_avancee.feature_engineering import prepare_features_with_io
 
 
@@ -21,13 +21,12 @@ def predict():
                                  features_path=features_path,
                                  training_mode=False)
         return features_path
-
-    # Start completing predict task
-    # predict = PythonOperator()
-    # End completing predict task
-
-    # feature_path = prepare_features_with_io_task()
-    # predict_with_io_task(feature_path=feature_path)
-
+    
+    @task
+    def predict_task(feature_path: str) -> None:
+        predict_with_io_task(feature_path=feature_path, model_path=MODEL_PATH, predictions_folder=PREDICTIONS_FOLDER)
+    
+    feature_path = prepare_features_with_io_task()
+    predict_task(feature_path)
 
 predict_dag = predict()
